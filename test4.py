4b9903ccd5e3339bf9014910cd2f1b8859e0879c
futebol <julioborges05@hotmail.com>
2023-11-02 20:03:19 -0300

    Enables the neural network on project

--- test4.py
+++ test4.py
@@ -97,23 +97,26 @@ def select_action(state):
     eps_threshold = EPS_END + (EPS_START - EPS_END) * \
         math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
-    # if sample > eps_threshold:
-    #     with torch.no_grad():
-    #         # t.max(1) will return the largest column value of each row.
-    #         # second column on max result is index of where max element was
-    #         # found, so we pick action with the larger expected reward.
-    #         # return policy_net(state).max(1)[1].view(1, 1)
-    # else:
-    return torch.from_numpy(env.action_space.sample())
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return torch.from_numpy(policy_net(state).numpy()[0])
+    else:
+        return torch.from_numpy(env.action_space.sample())
         # return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
 
 episode_durations = []
+episode_rewards = []
 
 
 def plot_durations(show_result=False):
     plt.figure(1)
     durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    rewards = torch.tensor(episode_rewards, dtype=torch.float)
+
     if show_result:
         plt.title('Result')
     else:
@@ -121,7 +124,10 @@ def plot_durations(show_result=False):
         plt.title('Training...')
     plt.xlabel('Episode')
     plt.ylabel('Duration')
+
     plt.plot(durations_t.numpy())
+    plt.plot(rewards.numpy())
+
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
         means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
@@ -136,51 +142,51 @@ def plot_durations(show_result=False):
         else:
             display.display(plt.gcf())
 
-def optimize_model():
-    if len(memory) < BATCH_SIZE:
-        return
-    transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). This converts batch-array of Transitions
-    # to Transition of batch-arrays.
-    batch = Transition(*zip(*transitions))
-
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                                if s is not None])
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
-
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1)[0].
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
-    next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    with torch.no_grad():
-        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
-    # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    # Compute Huber loss
-    criterion = nn.SmoothL1Loss()
-    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    # In-place gradient clipping
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
-    optimizer.step()
+# def optimize_model():
+#     if len(memory) < BATCH_SIZE:
+#         return
+#     transitions = memory.sample(BATCH_SIZE)
+#     # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+#     # detailed explanation). This converts batch-array of Transitions
+#     # to Transition of batch-arrays.
+#     batch = Transition(*zip(*transitions))
+#
+#     # Compute a mask of non-final states and concatenate the batch elements
+#     # (a final state would've been the one after which simulation ended)
+#     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+#                                           batch.next_state)), device=device, dtype=torch.bool)
+#     non_final_next_states = torch.cat([s for s in batch.next_state
+#                                                 if s is not None])
+#     state_batch = torch.cat(batch.state)
+#     action_batch = torch.cat(batch.action)
+#     reward_batch = torch.cat(batch.reward)
+#
+#     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+#     # columns of actions taken. These are the actions which would've been taken
+#     # for each batch state according to policy_net
+#     state_action_values = policy_net(state_batch).gather(1, action_batch.type(torch.int64))
+#
+#     # Compute V(s_{t+1}) for all next states.
+#     # Expected values of actions for non_final_next_states are computed based
+#     # on the "older" target_net; selecting their best reward with max(1)[0].
+#     # This is merged based on the mask, such that we'll have either the expected
+#     # state value or 0 in case the state was final.
+#     next_state_values = torch.zeros(BATCH_SIZE, device=device)
+#     with torch.no_grad():
+#         next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+#     # Compute the expected Q values
+#     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+#
+#     # Compute Huber loss
+#     criterion = nn.SmoothL1Loss()
+#     loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+#
+#     # Optimize the model
+#     optimizer.zero_grad()
+#     loss.backward()
+#     # In-place gradient clipping
+#     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+#     optimizer.step()
 
 if torch.cuda.is_available():
     num_episodes = 600
@@ -189,6 +195,8 @@ else:
 
 for i_episode in range(num_episodes):
     print(i_episode)
+
+    episode_reward = 0
 
     # Initialize the environment and get it's state
     state = env.reset()
@@ -201,6 +209,7 @@ for i_episode in range(num_episodes):
 
         # next_state, reward, terminated, _ = env.step(action)
         reward = torch.tensor([reward], device=device)
+        episode_reward += reward
         done = terminated
 
         if terminated:
@@ -227,8 +236,11 @@ for i_episode in range(num_episodes):
 
         if done:
             episode_durations.append(t + 1)
+            episode_rewards.append(episode_reward.numpy()[0])
             plot_durations()
+            print("episode reward: ", episode_reward.numpy()[0])
             break
+
 
 print('Complete')
 plot_durations(show_result=True)
