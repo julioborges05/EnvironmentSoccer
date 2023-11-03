f3dbd52230a2606dc0e3c1746306e1fc28d411de
futebol <julioborges05@hotmail.com>
2023-11-03 12:44:00 -0300

    Adds optimer method

--- test4.py
+++ test4.py
@@ -5,6 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from collections import namedtuple, deque
 from itertools import count
+import numpy as np
 
 import rsoccer_gym
 
@@ -87,9 +88,9 @@ def select_action(state):
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
-            return torch.from_numpy(policy_net(state).numpy()[0])
+            return policy_net(state)
     else:
-        return torch.from_numpy(env.action_space.sample())
+        return torch.tensor(np.array([env.action_space.sample()]))
 
 
 def plot_rewards(show_result=False):
@@ -108,7 +109,36 @@ def plot_rewards(show_result=False):
 
     plt.pause(0.001)  # pause a bit so that plots are updated
 
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
 
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # state_action_values = policy_net(state_batch).gather(1, action_batch)
+    state_action_values = policy_net(state_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
 
 episode_rewards = []
 
@@ -127,9 +157,10 @@ for i_episode in range(num_episodes):
     state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
     for t in count():
         action = select_action(state)
-        observation, reward, terminated, _ = env.step(action.numpy())
+        observation, reward, terminated, _ = env.step(action.numpy()[0])
 
         # env.render()
+        # print(reward)
 
         reward = torch.tensor([reward], device=device)
         episode_reward += reward
@@ -142,6 +173,7 @@ for i_episode in range(num_episodes):
 
         memory.push(state, action, next_state, reward)
         state = next_state
+        optimize_model()
 
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
